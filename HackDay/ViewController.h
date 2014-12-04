eadb01b80ebd4b1ceaa94651d8231c2259922042
Renaud CODARINI <rcodarini@groupe-exp.com>
2014-12-04 08:47:47 +0100

    Initial Commit

---
+++ HackDay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackDay
+//
+//  Created by rcodarini on 04/12/2014.
+//  Copyright (c) 2014 CodeMaison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HackDay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HackDay
+//
+//  Created by rcodarini on 04/12/2014.
+//  Copyright (c) 2014 CodeMaison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
